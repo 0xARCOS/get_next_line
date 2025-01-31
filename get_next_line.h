813cd3504a14ba054f8e126c9ebb90401d600c68
Ariel <129072087+V1nt3r4@users.noreply.github.com>
2025-01-31 13:32:45 +0100

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aarcos <aarcos@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/16 02:14:52 by aarcos            #+#    #+#             */
+/*   Updated: 2025/01/31 13:08:45 by aarcos           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+
+char	*get_next_line(int fd);
+char	*get_line2(char *remainder);
+char	*update_remainder(char *remainder);
+int		read_to_remainder(char **remainder, int fd, char *buffer);
+
+
+char	*ft_strchr(char *s, int c);
+size_t	ft_strlen(char *s);
+char	*ft_strjoin(char *s1, char *s2);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aarcos <aarcos@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/16 02:14:46 by aarcos            #+#    #+#             */
+/*   Updated: 2025/01/31 13:02:23 by aarcos           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(char *s, int c)
+{
+	if (!s)
+		return (NULL);
+	while (*s)
+	{
+		if (*s == (char)c)
+		{
+			return ((char *)s);
+		}
+		s++;
+	}
+	if (*s == (char)c)
+		return ((char *)s);
+	return (NULL);
+}
+
+// char	*ft_strjoin(char *s1, char *s2)
+// {
+// 	char	*result;
+// 	size_t	i;
+// 	size_t	j;
+// 	size_t	s1_len;
+// 	size_t	s2_len;
+
+// 	if(!s1)
+// 		return (ft_strjoin(s2, ""));
+// 	s1_len = ft_strlen(s1);
+// 	s2_len = ft_strlen(s2);
+// 	result = (char *)malloc(s1_len + s2_len + 1);
+// 	if (!result)
+// 		return (NULL);
+// 	i = 0;
+// 	while (i < s1_len)
+// 	{
+// 		result[i] = s1[i];
+// 		i++;
+// 	}
+// 	j = 0;
+// 	while (j < s2_len)
+// 	{
+// 		result[i + j] = s2[j];
+// 		j++;
+// 	}
+// 	result[i + j] = '\0';
+// 	return (result);
+// }
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*result;
+	size_t	i;
+	size_t	j;
+	size_t	s1_len;
+	size_t	s2_len;
+
+	if (!s1)
+		return (ft_strjoin(s2, ""));
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	result = (char *)malloc(s1_len + s2_len + 1);
+	if (!result)
+		return (NULL);
+	i = -1;
+	while (++i < s1_len)
+		result[i] = s1[i];
+	j = -1;
+	while (++j < s2_len)
+		result[i + j] = s2[j];
+	result[i + j] = '\0';
+	return (result);
+}
