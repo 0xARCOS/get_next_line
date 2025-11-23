e5ec75d470a631c7ef1add42d5a12f674cf9d5d7
0xARCOS <arielsarcos07@gmail.com>
2025-11-23 17:23:16 +0100

    Sólo falta Norminette

--- get_next_line.c
+++ get_next_line.c
@@ -1,128 +1,99 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ariarcos <ariarcos@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/12 05:14:23 by ariarcos          #+#    #+#             */
-/*   Updated: 2025/11/12 19:51:23 by ariarcos         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	ft_manejar_memoria(char **buffer_acumulado, char *nl_ptr, size_t line_len)
+static int	ft_handle_memory(char **acc_buffer, char *nl_ptr, size_t line_len)
 {
-	char	*p_old;
-	size_t	len_residuo;
+	char	*old_ptr;
+	size_t	residue_len;
 
 	if (!nl_ptr)
 	{
-		free(*buffer_acumulado);
-		*buffer_acumulado = NULL;
-		return (1);
+		free(*acc_buffer);
+		*acc_buffer = NULL;
+		return 1;
 	}
-	p_old = *buffer_acumulado;
-	len_residuo = ft_strlen(p_old) - line_len;
-	if (len_residuo > 0)
+	old_ptr = *acc_buffer;
+	residue_len = ft_strlen(old_ptr) - line_len;
+	if (residue_len > 0)
 	{
-		*buffer_acumulado = ft_substr(p_old, line_len, len_residuo);
-		if (!*buffer_acumulado)
-		{
-			free(p_old);
-			return (-1);
-		}
+		*acc_buffer = ft_substr(old_ptr, line_len, residue_len);
+		if (!*acc_buffer)
+			return (free(old_ptr), -1);
 	}
 	else
-		*buffer_acumulado = NULL;
-	free(p_old);
-	return (1);
+		*acc_buffer = NULL;
+	free(old_ptr);
+	return 1;
 }
 
-char	*ft_extraer_y_limpiar(char **buffer_acumulado)
+static char	*ft_extract_and_clean(char **acc_buffer)
 {
 	char	*nl_ptr;
 	char	*line;
 	size_t	line_len;
 	int		status;
 
-	if (!*buffer_acumulado)
+	if (!*acc_buffer)
 		return (NULL);
-	nl_ptr = ft_strchr(*buffer_acumulado, '\n');
+	nl_ptr = ft_strchr(*acc_buffer, '\n');
 	if (nl_ptr)
-		line_len = (nl_ptr - *buffer_acumulado) + 1;
+		line_len = (nl_ptr - *acc_buffer) + 1;
 	else
-		line_len = ft_strlen(*buffer_acumulado);
+		line_len = ft_strlen(*acc_buffer);
 	if (line_len == 0)
-	{
-		free(*buffer_acumulado);
-		*buffer_acumulado = NULL;
-		return (NULL);
-	}
-	line = ft_substr(*buffer_acumulado, 0, line_len);
+		return (free(*acc_buffer), *acc_buffer = NULL, NULL);
+	line = ft_substr(*acc_buffer, 0, line_len);
 	if (!line)
 		return (NULL);
-	status = ft_manejar_memoria(buffer_acumulado, nl_ptr, line_len);
+	status = ft_handle_memory(acc_buffer, nl_ptr, line_len);
 	if (status == -1)
 		return (free(line), NULL);
 	return (line);
 }
 
-static int	ft_error(char **buffer_acumulado, char *buf)
+static int	ft_read_data(int fd, char **acc_buffer)
 {
-	free(buf);
-	if (*buffer_acumulado)
-	{
-		free(*buffer_acumulado);
-		*buffer_acumulado = NULL;
-	}
-	return (-1);
-}
+	char	*buf;
+	ssize_t	bytes_read;
+	char	*temp;
 
-int	ft_leer_datos(int fd, char **buffer_acumulado)
-{
-	char		*buf;
-	ssize_t		bytes_read;
-	char		*temp;
-
-	buf = malloc(BUFFER_SIZE + 1);
+	if (*acc_buffer && ft_strchr(*acc_buffer, '\n'))
+		return 1;
+	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buf)
-		return (-1);
-	while (!(*buffer_acumulado && ft_strchr(*buffer_acumulado, '\n')))
-	{
-		bytes_read = read(fd, buf, BUFFER_SIZE);
-		if (bytes_read == -1)
-			return (ft_error(buffer_acumulado, buf));
-		if (bytes_read == 0)
-			return (free(buf), 0);
-		buf[bytes_read] = '\0';
-		temp = *buffer_acumulado;
-		*buffer_acumulado = ft_strjoin(*buffer_acumulado, buf);
+		return -1;
+	bytes_read = read(fd, buf, BUFFER_SIZE);
+	if (bytes_read == -1)
+		return (free(buf), -1);
+	if (bytes_read == 0)
+		return (free(buf), 0);
+	buf[bytes_read] = '\0';
+	temp = *acc_buffer;
+	*acc_buffer = ft_strjoin(*acc_buffer, buf);
+	free(buf);
+	if (temp)
 		free(temp);
-		if (!*buffer_acumulado)
-			return (free(buf), -1);
-	}
-	return (free(buf), 1);
+	if (!*acc_buffer)
+		return -1;
+	return ft_read_data(fd, acc_buffer);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*buffer_residuo[1024];
+	static char	*residue_buffer;
 	char		*final_line;
-	int			estado;
+	int			status;
 
-	if (fd < 0 || fd >= 1024)
+	if (fd < 0)
 		return (NULL);
-	estado = ft_leer_datos(fd, &buffer_residuo[fd]);
-	if (estado == -1)
+	status = ft_read_data(fd, &residue_buffer);
+	if (status == -1 || (status == 0 && !residue_buffer))
 	{
-		free(buffer_residuo[fd]);
-		buffer_residuo[fd] = NULL;
+		if (residue_buffer)
+			free(residue_buffer);
+		residue_buffer = NULL;
 		return (NULL);
 	}
-	if (estado == 0 && !buffer_residuo[fd])
-		return (NULL);
-	final_line = ft_extraer_y_limpiar(&buffer_residuo[fd]);
+	final_line = ft_extract_and_clean(&residue_buffer);
 	return (final_line);
 }
---
+++ get_next_line_bonus.c
@@ -0,0 +1,128 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ariarcos <ariarcos>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/01 00:00:00 by tu_login          #+#    #+#             */
+/*   Updated: 2025/11/23 14:17:58 by ariarcos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+static int	ft_handle_memory(char **acc_buffer, char *nl_ptr, size_t line_len)
+{
+	char	*old_ptr;
+	size_t	residue_len;
+
+	if (!nl_ptr)
+	{
+		free(*acc_buffer);
+		*acc_buffer = NULL;
+		return (1);
+	}
+	old_ptr = *acc_buffer;
+	residue_len = ft_strlen(old_ptr) - line_len;
+	if (residue_len > 0)
+	{
+		*acc_buffer = ft_substr(old_ptr, line_len, residue_len);
+		if (!*acc_buffer)
+			return (free(old_ptr), -1);
+	}
+	else
+		*acc_buffer = NULL;
+	free(old_ptr);
+	return (1);
+}
+
+static char	*ft_extract_and_clean(char **acc_buffer)
+{
+	char	*nl_ptr;
+	char	*line;
+	size_t	line_len;
+	int		status;
+
+	if (!*acc_buffer)
+		return (NULL);
+	nl_ptr = ft_strchr(*acc_buffer, '\n');
+	if (nl_ptr)
+		line_len = (nl_ptr - *acc_buffer) + 1;
+	else
+		line_len = ft_strlen(*acc_buffer);
+	if (line_len == 0)
+		return (free(*acc_buffer), *acc_buffer = NULL, NULL);
+	line = ft_substr(*acc_buffer, 0, line_len);
+	if (!line)
+		return (NULL);
+	status = ft_handle_memory(acc_buffer, nl_ptr, line_len);
+	if (status == -1)
+		return (free(line), NULL);
+	return (line);
+}
+
+static int	ft_read_data(int fd, char **acc_buffer)
+{
+	char	*buf;
+	ssize_t	bytes_read;
+	char	*temp;
+
+	if (*acc_buffer && ft_strchr(*acc_buffer, '\n'))
+		return (1);
+	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buf)
+		return (-1);
+	bytes_read = read(fd, buf, BUFFER_SIZE);
+	if (bytes_read == -1)
+		return (free(buf), -1);
+	if (bytes_read == 0)
+		return (free(buf), 0);
+	buf[bytes_read] = '\0';
+	temp = *acc_buffer;
+	*acc_buffer = ft_strjoin(*acc_buffer, buf);
+	free(buf);
+	if (temp)
+		free(temp);
+	if (!*acc_buffer)
+		return (-1);
+	return (ft_read_data(fd, acc_buffer));
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*residue_buffer[1024];
+	char		*final_line;
+	int			status;
+
+	if (fd < 0 || fd >= 1024)
+		return (NULL);
+	status = ft_read_data(fd, &residue_buffer[fd]);
+	if (status == -1 || (status == 0 && !residue_buffer[fd]))
+	{
+		if (residue_buffer[fd])
+		{
+			free(residue_buffer[fd]);
+			residue_buffer[fd] = NULL;
+		}
+		return (NULL);
+	}
+	final_line = ft_extract_and_clean(&residue_buffer[fd]);
+	return (final_line);
+}
+
+// int main(void)
+// {
+//     int fd;
+//     char *line;
+//     fd = open("file.txt", O_RDONLY);
+//     // int fd2 = open("get_next_line.c", O_RDONLY);
+//     if (fd == -1)
+//     {
+//         perror("Error al abrir el archivo");
+//         return 1;
+//     }
+//     printf("%s\n", get_next_line(0));
+//     close(fd);
+//     return 0;
+// }
